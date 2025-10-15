f0c1594b493fa259f040a656616047b75861bd62
Darsh Mohile <devmahida340@gmail.com>
2025-10-15 11:01:47 +0000

    authenticaion code changes 2

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+
+router.get('/me', isAuthenticated, (req, res) => {
+    // req.user is set by Passport deserializeUser
+
+    console.log(req.user.fName);
+
+    res.status(200).json({
+        fName: req.user.fName,
+        lName: req.user.lName,
+        email: req.user.email
+    });
+});
+
+module.exports = router;
