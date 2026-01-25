f1ae45fbf13f8c0faf7e1718aa16ade9e1e52751
Darsh Mohile <devmahida340@gmail.com>
2026-01-25 13:48:01 +0000

    added /fetchEvents and /registerEvent

--- routes/events.js
+++ routes/events.js
@@ -43,13 +43,48 @@ router.get('/getEvents', jwtAuth, async(req, res) => {
 
     try
     {
-        
+        const events = await eventModel.find();
+        res.status(200).json(events);
     }
     catch(e)
     {
         console.log('error sending event details: ', e.message);
         res.status(500).json('something went wrong');
     }
+});
+
+
+router.post('/registerEvent/:eventID', jwtAuth, async(req, res) => {
+
+    try
+    {
+        const eventID = req.params.eventID;
+        const uid = req.user._id;
+
+        const result = await eventModel.updateOne(
+            {
+                _id: eventID,
+                participants: { $ne: uid },
+                $expr: { $lt: [{ $size: "$participants" }, "$maxParticipants"] }
+            },
+            {
+                $addToSet: { participants: uid }
+            }
+        );
+
+        if (result.matchedCount === 0)
+        {
+            return res.status(400).json('Event not found, already registered, or event is full');
+        }
+
+        return res.status(200).json('Event registered successfully');
+    }
+    catch(err)
+    {
+        console.log('::Error registering for event:', err.message);
+        res.status(500).json('something went wrong');
+    }
+
 });
 
 
